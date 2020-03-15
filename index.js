9e379d4bffcf2661f5416554e448c4f2ade750cb
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-03-16 02:03:47 +0800

    [refactor] Redirect homepage to github page

--- index.js
+++ index.js
@@ -13,19 +13,21 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(cache('1 hour'));
 
-app.get('/cases', async (req, res) => {
+app.get('/', (_, res) => res.redirect('https://github.com/sorxrob/coronavirus-ph-api'));
+
+app.get('/cases', async (_, res) => {
     const scraper = new Scraper();
     const data = await scraper.getCases();
     return res.json(data);
 });
 
-app.get('/cases-outside-ph', async (req, res) => {
+app.get('/cases-outside-ph', async (_, res) => {
     const scraper = new Scraper();
     const data = await scraper.getCasesOutsidePh();
     return res.json(data);
 });
 
-app.get('/suspected-cases', async (req, res) => {
+app.get('/suspected-cases', async (_, res) => {
     const scraper = new Scraper();
     const data = await scraper.getSuspectedCases();
     return res.json(data);
