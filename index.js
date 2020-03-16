ccc42b902d13f4e9ac3da9b45b6c422a8e033136
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-03-16 20:52:42 +0800

    [refactor] Append TBA cases

--- index.js
+++ index.js
@@ -16,29 +16,29 @@ app.use(cors());
 app.use(cache('1 hour'));
 
 app.get('/', (_, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/cases', async (_, res) => {
-    const data = await scrape.getCases();
-    return res.json(data);
+  const data = await scrape.getCases();
+  return res.json(data);
 });
 
 app.get('/cases-outside-ph', async (_, res) => {
-    const data = await scrape.getCasesOutsidePh();
-    return res.json(data);
+  const data = await scrape.getCasesOutsidePh();
+  return res.json(data);
 });
 
 app.get('/suspected-cases', async (_, res) => {
-    const data = await scrape.getSuspectedCases();
-    return res.json(data);
+  const data = await scrape.getSuspectedCases();
+  return res.json(data);
 });
 
 app.get('/patients-under-investigation', async (_, res) => {
-    const data = await scrape.getPatientsUnderInvestigation();
-    return res.json(data);
+  const data = await scrape.getPatientsUnderInvestigation();
+  return res.json(data);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}...`);
+  console.log(`Server started on port ${PORT}...`);
 });
--- services/Scraper.js
+++ services/Scraper.js
@@ -5,111 +5,132 @@ const axios = require('axios');
 const URL = 'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_the_Philippines';
 
 class Scraper {
-    async getHTML() {
-        const res = await axios(URL);
-        return cheerio.load(res.data);
+  async getHTML() {
+    const res = await axios(URL);
+    return cheerio.load(res.data);
+  }
+  
+  async getCases() {
+    const $ = await this.getHTML();
+    cheerioTableparser($);
+    const rawData = $('.wikitable').first().parsetable(true, true, true);
+    
+    const formattedData = [];
+    
+    rawData[0].forEach((item, idx) => {
+      if (idx === 0) return;
+      
+      const obj = {
+        "case_no": +item,
+        "date": rawData[1][idx],
+        "age": +rawData[2][idx],
+        "gender": rawData[3][idx],
+        "nationality": rawData[4][idx],
+        "hospital_admitted_to": rawData[5][idx],
+        "had_recent_travel_history_abroad": rawData[6][idx],
+        "status": rawData[7][idx],
+        "notes": rawData[8][idx]
+      };
+      
+      formattedData.push(obj);
+    });
+    
+    // Infobox data can be updated, but it needs the complete data before
+    // it being added to the confirmed cases table, so we add TBA values
+    const confirmedCases = $('.infobox tbody tr th:contains("Confirmed cases")').next().text();
+    if (+confirmedCases > formattedData.length) {
+      console.log('yes')
+      const diff = +confirmedCases - formattedData.length;
+      for (let x = 0; x < diff; x++) {
+        formattedData.push({
+          "case_no": formattedData.length + 1,
+          "date": "TBA",
+          "age": "TBA",
+          "gender": "TBA",
+          "nationality": "TBA",
+          "hospital_admitted_to": "TBA",
+          "had_recent_travel_history_abroad": "TBA",
+          "status": "TBA",
+          "notes": "TBA"
+        });
+      }
     }
     
-    async getCases() {
-        const $ = await this.getHTML();
-        cheerioTableparser($);
-        const rawData = $('.wikitable').first().parsetable(true, true, true);
-        
-        const formattedData = [];
-        
-        rawData[0].forEach((item, idx) => {
-            if (idx === 0) return;
-            
-            const obj = {
-                "case_no": +item,
-                "date": rawData[1][idx],
-                "age": +rawData[2][idx],
-                "gender": rawData[3][idx],
-                "nationality": rawData[4][idx],
-                "hospital_admitted_to": rawData[5][idx],
-                "had_recent_travel_history_abroad": rawData[6][idx],
-                "status": rawData[7][idx],
-                "notes": rawData[8][idx]
-            };
-            
-            formattedData.push(obj);
-        });
-        
-        return formattedData;
+    return formattedData;
+  }
+  
+  async getCasesOutsidePh() {
+    const $ = await this.getHTML();
+    cheerioTableparser($);
+    const rawData = $('.wikitable').eq(1).parsetable(true, true, true);
+    
+    const formattedData = [];
+    
+    rawData[0].forEach((item, idx) => {
+      const skip = [0, rawData[0].length - 1, rawData[0].length - 2]
+      if (skip.includes(idx)) return;
+      
+      const obj = {
+        "country_territory_place": item,
+        "confirmed": +rawData[1][idx],
+        "recovered": +rawData[2][idx],
+        "died": +rawData[3][idx]
+      };
+      
+      formattedData.push(obj);
+    });
+    
+    return formattedData;
+  }
+  
+  async getSuspectedCases() {
+    const $ = await this.getHTML();
+    cheerioTableparser($);
+    const rawData = $('.wikitable').eq(3).parsetable(true, true, true);
+    
+    return {
+      "confirmed_cases": +rawData[1][0],
+      "cases_tested_negative": +rawData[1][1],
+      "cases_pending_test_results": +rawData[1][2]
     }
-
-    async getCasesOutsidePh() {
-        const $ = await this.getHTML();
-        cheerioTableparser($);
-        const rawData = $('.wikitable').eq(1).parsetable(true, true, true);
-        
-        const formattedData = [];
-        
-        rawData[0].forEach((item, idx) => {
-            const skip = [0, rawData[0].length - 1, rawData[0].length - 2]
-            if (skip.includes(idx)) return;
-            
-            const obj = {
-                "country_territory_place": item,
-                "confirmed": +rawData[1][idx],
-                "recovered": +rawData[2][idx],
-                "died": +rawData[3][idx]
-            };
-            
-            formattedData.push(obj);
-        });
-        
-        return formattedData;
-    }
-
-    async getSuspectedCases() {
-        const $ = await this.getHTML();
-        cheerioTableparser($);
-        const rawData = $('.wikitable').eq(3).parsetable(true, true, true);
-
-        return {
-            "confirmed_cases": +rawData[1][0],
-            "cases_tested_negative": +rawData[1][1],
-            "cases_pending_test_results": +rawData[1][2]
-        }
-    }
-
-    async getPatientsUnderInvestigation() {
-        const $ = await this.getHTML();
-        cheerioTableparser($);
-        const rawData = $('.wikitable').eq(4).parsetable(true, true, true);
-
-        const formattedData = [];
-        
-        rawData[0].forEach((item, idx) => {
-            const skip = [0, 1, 2, rawData[0].length - 1, rawData[0].length - 2, rawData[0].length - 3];
-            if (skip.includes(idx)) return;
-            
-            const obj = {
-                "region": item,
-                "current_pui_status": {
-                    "suspected_cases": {
-                        "admitted": +rawData[1][idx],
-                        "deaths": +rawData[2][idx]
-                    },
-                    "confirmed_cases": {
-                        "admitted": +rawData[3][idx],
-                        "recoveries": +rawData[4][idx],
-                        "deaths": +rawData[5][idx],
-                    }
-                },
-                "total": 0
-            };
-
-            for (let x = 1; x <= 5; x++) {
-                obj.total += +rawData[x][idx];
-            }
-            
-            formattedData.push(obj);
-        });
-        
-        return formattedData;
-    }
+  }
+  
+  async getPatientsUnderInvestigation() {
+    const $ = await this.getHTML();
+    cheerioTableparser($);
+    const rawData = $('.wikitable').eq(4).parsetable(true, true, true);
+    
+    const formattedData = [];
+    
+    rawData[0].forEach((item, idx) => {
+      const skip = [0, 1, 2, rawData[0].length - 1, rawData[0].length - 2, rawData[0].length - 3];
+      if (skip.includes(idx)) return;
+      
+      const obj = {
+        "region": item,
+        "current_pui_status": {
+          "suspected_cases": {
+            "admitted": +rawData[1][idx],
+            "deaths": +rawData[2][idx]
+          },
+          "confirmed_cases": {
+            "admitted": +rawData[3][idx],
+            "recoveries": +rawData[4][idx],
+            "deaths": +rawData[5][idx],
+          }
+        },
+        "total": 0
+      };
+      
+      for (let x = 1; x <= 5; x++) {
+        obj.total += +rawData[x][idx];
+      }
+      
+      formattedData.push(obj);
+    });
+    
+    return formattedData;
+  }
 }
 
 module.exports = Scraper;
