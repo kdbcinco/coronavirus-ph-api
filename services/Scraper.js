8ec562a4d87c4e4a54e406c5aec316ae835b409f
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-03-26 10:10:39 +0800

    [fix] Fix null value in cases outside ph

--- services/Scraper.js
+++ services/Scraper.js
@@ -209,9 +209,9 @@ class Scraper {
 
       const obj = {
         country_territory_place: item,
-        confirmed: +rawData[1][idx],
-        recovered: +rawData[2][idx],
-        died: +rawData[3][idx]
+        confirmed: +rawData[1][idx].split('[')[0],
+        recovered: +rawData[2][idx].split('[')[0],
+        died: +rawData[3][idx].split('[')[0]
       }
 
       formattedData.push(obj)
