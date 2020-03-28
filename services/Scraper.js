5691752ec98720e8201450daf57d7118b9e88c4f
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-03-28 21:01:38 +0800

    [fix] Fix cases outside ph

--- services/Scraper.js
+++ services/Scraper.js
@@ -210,7 +210,7 @@ class Scraper {
     const $ = await this.getHTML()
     cheerioTableparser($)
     const rawData = $('.wikitable')
-      .eq(1)
+      .eq(2)
       .parsetable(true, true, true)
 
     const formattedData = []
